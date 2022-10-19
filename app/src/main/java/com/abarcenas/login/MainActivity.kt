bd8dc313d538852b64b558b4dce3cc2029bc044a
ALEJANDRO B <abarcenas05@gmail.com>
2022-10-19 16:10:29 -0500

    add

--- app/src/main/java/com/abarcenas/login/MainActivity.kt
+++ app/src/main/java/com/abarcenas/login/MainActivity.kt
@@ -3,40 +3,36 @@ package com.abarcenas.login
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast.LENGTH_LONG
+import com.abarcenas.login.databinding.ActivityMainBinding
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-       //Utilizamos la funcion setOnClickListener para cuando aplastemos el boton
-        button2.setOnClickListener{
-            val username = tusername.text.toString().trim() //Declaramos la variable username al id de editor de texto llamda tusername
-            val password = tpassword.text.toString().trim() //Declaramos la variable username al id de editor de texto llamada tpassword
 
-            if(username.isEmpty())
-            {
-                tusername.error = "Introduzca su usuario"
-                tusername.requestFocus()
-            }
-            if (password.isEmpty()){
-                tpassword.error = "Introduzca su contraseña"
-                tpassword.requestFocus()
-            }
-            if(username.toString() != "ale" && password.toString() != "12345") //Si la contraseña y el pasworrd son los establecidos
-            {                                                                  // nos permitira continuar y pasar al siguiente paso
-            }else
-            {
-             val intent = Intent(this,HomeActivity::class.java)
-              startActivity(intent)
+
+
+        binding.button2.setOnClickListener {
+            if (binding.tpassword.text.toString().isBlank() || binding.tusername.text.toString().isBlank()) {
+                Toast.makeText(baseContext, "agregue datos de usuario y contraseña", LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
             }
         }
-
     }
 }
